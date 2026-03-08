9b7689689a5ce230c6795841ddd032bedbd766a4
rebekahpatel <rebapatel@gmail.com>
2026-03-08 15:36:14 -0700

    Finalizing first tab, Variable Exploration, of the R Shiny App

--- hw4/mimiciv_shiny/app.R
+++ hw4/mimiciv_shiny/app.R
@@ -1,20 +1,106 @@
 library(shiny)
+library(bslib)
+library(ggplot2)
+library(gtsummary)
+library(gt)
 
-ui <- pageWithSidebar(
+mimic_icu_cohort <- readRDS("mimic_icu_cohort.rds")
+
+ui <- navbarPage(
+  title = "Exploring the Mimic Dataset",
+  theme = bs_theme(version = 5),
   
-  # App title 
-  headerPanel("Exploring the Mimic Dataset"),
+  tabPanel("Variable Exploration",
+    sidebarLayout(
+      sidebarPanel(
+        card(
+            # which graph should be shown
+            selectInput(
+              inputId = "var",
+              label = "Lab/Vital/Demographic Summary:",
+              choices = c("hematocrit", "creatinine", "chloride", "sodium",
+                          "glucose", "bicarbonate", "wbc", "potassium",
+                          "heart_rate", "temperature_fahrenheit",
+                          "non_invasive_blood_pressure_diastolic",
+                          "respiratory_rate",
+                          "non_invasive_blood_pressure_systolic",
+                          "los", "insurance", "language",
+                          "marital_status", "race", "gender"),
+              selected = "hematocrit"
+            )
+        )
+      ),
+      
+      # Main panel for displaying outputs
+      mainPanel(
+        card(
+          plotOutput(outputId = "barplot")
+        ),
+        
+        card(
+          gt_output(outputId = "my_gt_table")
+        )
+      )
+    )
+  ),
   
-  # Sidebar panel for inputs
-  sidebarPanel(),
-  
-  # Main panel for displaying outputs
-  mainPanel()
+  tabPanel("Patient Exploration",
+    sidebarLayout(
+      sidebarPanel(),
+      mainPanel()
+    )
+  )
 )
 
+categorical = c("insurance", "language",
+         "marital_status", "race", "gender")
+
+continuous = c("hematocrit", "creatinine", "chloride", "sodium",
+         "glucose", "bicarbonate", "wbc", "potassium", "los",
+         "heart_rate", "temperature_fahrenheit",
+         "non_invasive_blood_pressure_diastolic",
+         "respiratory_rate",
+         "non_invasive_blood_pressure_systolic")
+
 server <- function(input, output) {
+  
+  output$barplot <- renderPlot({
+    
+    if (input$var %in% categorical) {
+      ggplot(data = mimic_icu_cohort, 
+             aes(x = .data[[input$var]])) +
+        geom_bar(fill = "steelblue") +
+        theme_minimal() +
+        labs(title = paste("Distribution of ", input$var), 
+             y = "Count") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    } 
+    else {
+      ggplot(data = mimic_icu_cohort,
+             aes(x = .data[[input$var]])) +
+      geom_boxplot(fill = "orange") +
+      theme_minimal() +
+      labs(title = paste("Distribution of ", input$var),
+           y = "Count") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+        
+    }
+    
+  })
+  
+  output$my_gt_table <- 
+    render_gt({
+      mimic_icu_cohort %>%
+        tbl_summary(
+          include = input$var
+          ) %>%
+        add_n() %>%
+        as_gt() %>%
+        tab_header(md("**Table 1. Patient Characteristics**"))
+      
+    })
   
   
 }
 
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
